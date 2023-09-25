25af7d48b60ff6f94c27a93b407572f34d2039d4
Bers-AL <62203754+Bers-AL@users.noreply.github.com>
2023-09-25 15:42:53 +0300

    Update MyEntity.java
    Signed-off-by: Bers-AL <62203754+Bers-AL@users.noreply.github.com>

--- src/main/java/web/model/MyEntity.java
+++ src/main/java/web/model/MyEntity.java
@@ -1,12 +1,14 @@
 package web.model;
 
-import lombok.Getter;
-
-
 public class MyEntity {
     Long id;
 
     public Long getId() {
         return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
+
