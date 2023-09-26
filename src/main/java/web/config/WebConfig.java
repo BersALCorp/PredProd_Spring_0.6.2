ff9b04e26e8c6d4645310f1f876db5de0014938f
Bers-AL <Bers666@gmail.com>
2023-09-26 17:35:33 +0300

    PP 3.1.1 done

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -4,15 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @ComponentScan("web")
 @PropertySource("classpath:application.properties")
-@EnableJpaRepositories("web.repository")
 @EntityScan("web.model")
-@EnableTransactionManagement
 public class WebConfig {
 }
