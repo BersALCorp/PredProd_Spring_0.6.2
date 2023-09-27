c38fa715cc726e06cb1664124e7d4bbda66ed6d1
Bers-AL <62203754+Bers-AL@users.noreply.github.com>
2023-09-27 11:21:31 +0300

    Update MySpringBootApplication.java
    Signed-off-by: Bers-AL <62203754+Bers-AL@users.noreply.github.com>

--- src/main/java/web/MySpringBootApplication.java
+++ src/main/java/web/MySpringBootApplication.java
@@ -1,15 +1,15 @@
-//package web;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//
-//@SpringBootApplication
-//@EnableWebMvc
-//public class MySpringBootApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(MySpringBootApplication.class, args);
-//    }
-//}
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication
+@EnableWebMvc
+public class MySpringBootApplication {
+
+   public static void main(String[] args) {
+       SpringApplication.run(MySpringBootApplication.class, args);
+   }
+}
